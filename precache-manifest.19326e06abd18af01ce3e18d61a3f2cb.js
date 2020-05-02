e15bf75aefa82843d219ea8cea2e3aed78d4e15e
Holly <hharnack@hotmail.com>
2020-05-01 23:06:49 -0600

    Revert "Updates"
    This reverts commit 2d23f656ffcf39790abb3b2c79c08325e34eb2d0.

--- precache-manifest.19326e06abd18af01ce3e18d61a3f2cb.js
+++ precache-manifest.19326e06abd18af01ce3e18d61a3f2cb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "531529fd928c12e57acd522acc7348f7",
+    "revision": "fbe016e15a4af1c4bcdd517e10b0aa2c",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "64b698b2c407d4bdd36c",
+    "revision": "6612af4e5fc2acb97e3b",
     "url": "/portfolio/static/css/main.d1b05096.chunk.css"
   },
   {
-    "revision": "e07c9058315706347c14",
-    "url": "/portfolio/static/js/2.d22755f3.chunk.js"
+    "revision": "afda21390b6b4cb282a8",
+    "url": "/portfolio/static/js/2.262eeec0.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/portfolio/static/js/2.d22755f3.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.262eeec0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "64b698b2c407d4bdd36c",
-    "url": "/portfolio/static/js/main.f056302f.chunk.js"
+    "revision": "6612af4e5fc2acb97e3b",
+    "url": "/portfolio/static/js/main.678f174f.chunk.js"
   },
   {
     "revision": "ee3d72d161f98a5d4b9d",
