d9bab7748db2fe5123eca630a65dca4e7d2b11ac
Holly <hharnack@hotmail.com>
2020-05-03 12:34:01 -0600

    Adding sidebar and footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <sidebar>
 
         </sidebar>
@@ -18,7 +17,9 @@ function App() {
         <contact>
           
         </contact>
-      </header>
+        <footer>
+          
+        </footer>
     </div>
   );
 }
