9e4935493c292d74e51bf31b53eff7765c8112dc
Holly <hharnack@hotmail.com>
2020-05-05 02:14:57 -0600

    Updates

---
+++ precache-manifest.542183dfaaf712c907301c049f2cff95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1398141335254096f4950380ea8a3b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "412f2f80922d2d5fc440",
+    "url": "/portfolio/static/css/2.102fa471.chunk.css"
+  },
+  {
+    "revision": "27f378b1ee76a3aed7f6",
+    "url": "/portfolio/static/css/main.b775ac0c.chunk.css"
+  },
+  {
+    "revision": "412f2f80922d2d5fc440",
+    "url": "/portfolio/static/js/2.439cb819.chunk.js"
+  },
+  {
+    "revision": "d7186be027e5346a0a2588e7f8b6f9a4",
+    "url": "/portfolio/static/js/2.439cb819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f378b1ee76a3aed7f6",
+    "url": "/portfolio/static/js/main.2beb04b9.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  },
+  {
+    "revision": "13a73b1c5444cb7239adaf1b4eb65851",
+    "url": "/portfolio/static/media/HollyHarnackResume.13a73b1c.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f10dbd63ddcff7e127eff0014df30637.js"
+  "/portfolio/precache-manifest.542183dfaaf712c907301c049f2cff95.js"
 );
 
 self.addEventListener('message', (event) => {
