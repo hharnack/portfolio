7a8c4139afe7aed82d8495df9667ca4e4beb8205
Holly <hharnack@hotmail.com>
2020-05-11 23:38:16 -0600

    Update sidebar.js

--- src/sidebar.js
+++ src/sidebar.js
@@ -8,17 +8,31 @@ class Sidebar extends React.Component {
 
         return (
             <Container fluid className="sidebar d-none d-md-block">
-                <Row className="p-3 pt-5">
-                    <Col><a href="#about" className="sidebar-item">About Me</a></Col>
+                <Row>
+                    <Col>
+                        <Row className="p-3 pt-5">
+                            <Col><a href="#about" className="sidebar-item">About Me</a></Col>
+                        </Row>
+                        <Row className="sidebar-item p-3">
+                            <Col><a href="#proj" className="sidebar-item">Projects</a></Col>
+                        </Row>
+                        <Row className="sidebar-item p-3">
+                            <Col> <a href="#contact" className="sidebar-item">Contact Me</a></Col>
+                        </Row>
+                    </Col>
                 </Row>
-                <Row className="sidebar-item p-3">
-                    <Col><a href="#proj" className="sidebar-item">Projects</a></Col>
-                </Row>
-                <Row className="sidebar-item p-3">
-                    <Col> <a href="#contact" className="sidebar-item">Contact Me</a></Col>
-                </Row>
-                <Row className="sidebar-item p-3">
-                    <Col><a href={pdf} className="sidebar-item">Resume</a></Col>
+                <Row className="end">
+                    <Col className="align-self-end">
+                        <Row className="sidebar-item p-3">
+                            <Col><a href={pdf} className="sidebar-item">Resume</a></Col>
+                        </Row>
+                        <Row className="sidebar-item p-3">
+                            <Col><a href="" className="sidebar-item">Github</a></Col>
+                        </Row>
+                        <Row className="sidebar-item p-3">
+                            <Col><a href="" className="sidebar-item">Linked In</a></Col>
+                        </Row>
+                    </Col>
                 </Row>
             </Container>
         );
