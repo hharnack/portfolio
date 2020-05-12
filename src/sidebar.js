5206f81c1a3379118260caaf2a1e388c25098608
Holly <hharnack@hotmail.com>
2020-05-12 14:19:04 -0600

    Update sidebar.js

--- src/sidebar.js
+++ src/sidebar.js
@@ -7,34 +7,18 @@ class Sidebar extends React.Component {
     render() {
 
         return (
-            <Container fluid className="sidebar d-none d-md-block">
-                <Row>
-                    <Col>
-                        <Row className="p-3 pt-5">
-                            <Col><a href="#about" className="sidebar-item">About Me</a></Col>
-                        </Row>
-                        <Row className="sidebar-item p-3">
-                            <Col><a href="#proj" className="sidebar-item">Projects</a></Col>
-                        </Row>
-                        <Row className="sidebar-item p-3">
-                            <Col> <a href="#contact" className="sidebar-item">Contact Me</a></Col>
-                        </Row>
-                    </Col>
-                </Row>
-                <Row className="end">
-                    <Col className="align-self-end">
-                        <Row className="sidebar-item p-3">
-                            <Col><a href={pdf} className="sidebar-item">Resume</a></Col>
-                        </Row>
-                        <Row className="sidebar-item p-3">
-                            <Col><a href="https://github.com/hharnack/" className="sidebar-item">Github</a></Col>
-                        </Row>
-                        <Row className="sidebar-item p-3">
-                            <Col><a href="https://www.linkedin.com/in/hollyharnack/" className="sidebar-item">LinkedIn</a></Col>
-                        </Row>
-                    </Col>
-                </Row>
-            </Container>
+            <div className="sidebar d-none d-md-block">
+                <div style={{ height: "100%" }} className="d-flex flex-column align-items-start bd-highlight">
+                    <div className="mb-auto pt-4 d-flex flex-column">
+                        <a href="#about" className="p-2 bd-highlight sidebar-item">About Me</a>
+                        <a href="#proj" className="p-2 bd-highlight sidebar-item">Projects</a>
+                        <a href="#contact" className="p-2 bd-highlight sidebar-item">Contact Me</a>
+                    </div>
+                    <div className="p-2 bd-highlight"><a href={pdf} className="sidebar-item">Resume</a></div>
+                    <div className="p-2 bd-highlight"><a href="https://github.com/hharnack/" className="sidebar-item">Github</a></div>
+                    <div className="p-2 bd-highlight"><a href="https://www.linkedin.com/in/hollyharnack/" className="sidebar-item">LinkedIn</a></div>
+                </div>
+            </div>
         );
 
     }
