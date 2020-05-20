318bfea7b9401e55f2300bf0580352413a9182e6
Holly <hharnack@hotmail.com>
2020-05-19 23:18:24 -0600

    adding drop down

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import pdf from '../src/HollyHarnackResume.pdf';
 
 class Navbar extends React.Component {
 
@@ -14,32 +15,22 @@ class Navbar extends React.Component {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
-                            <a className="nav-link" href="#">About Me</a>
+                            <a className="nav-link" href="#about">About Me</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Projects</a>
+                            <a className="nav-link" href="#proj">Projects</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Contact Me</a>
+                            <a className="nav-link" href="#contact">Contact Me</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Resume</a>
+                            <a className="nav-link" href={{pdf}}>Resume</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Github</a>
+                            <a className="nav-link" href="https://github.com/hharnack/">Github</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">LinkedIn</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Dropdown</a>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a className="dropdown-item" href="#">Action</a>
-                                <a className="dropdown-item" href="#">Another action</a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="#">Something else here</a>
-                            </div>
+                            <a className="nav-link" href="https://www.linkedin.com/in/hollyharnack/">LinkedIn</a>
                         </li>
                     </ul>
                 </div>
