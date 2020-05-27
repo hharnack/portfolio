aed331996c7ce5534bc9cde2eb668dc8bd88a849
Holly <hharnack@hotmail.com>
2020-05-26 23:46:44 -0600

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,8 +5,10 @@ class Project extends React.Component{
     render() {
 
         return (
+            <div className="d-flex justify-content-center">
             <div className="page">
                Project 
+            </div>
             </div>
         );
 
