7a3f7a335516b20777b8fc93fdb0db47fd66a1c7
Holly <hharnack@hotmail.com>
2020-06-11 16:45:06 -0600

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,18 +8,15 @@ class Navbar extends React.Component {
         return (
             <nav className="navbar">
 
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <nav className="navbar navbar-expand-lg bg-dark">
+                    <div id="navbarNavAltMarkup">
                         <div className="navbar-nav container">
-                            <a className="nav-item nav-link active" href="#about">About Me <span className="sr-only">(current)</span></a>
+                            <a className="nav-item nav-link" href="#about">About Me</a>
                             <a className="nav-item nav-link" href="#proj">Projects</a>
                             <a className="nav-item nav-link" href="#contact">Contact Me</a>
-                            <a className="nav-item nav-link" href={pdf} className="sidebar-item">Resume</a>
-                            <a className="nav-item nav-link" href="https://github.com/hharnack/" className="sidebar-item">Github</a>
-                            <a className="nav-item nav-link" href="https://www.linkedin.com/in/hollyharnack/" className="sidebar-item">LinkedIn</a>
+                            <a className="nav-item nav-link" href={pdf}>Resume</a>
+                            <a className="nav-item nav-link" href="https://github.com/hharnack/">Github</a>
+                            <a className="nav-item nav-link" href="https://www.linkedin.com/in/hollyharnack/">LinkedIn</a>
                         </div>
                     </div>
                 </nav>
