265e540b6dcc3d2b6c42c73a0521a11d045fac2b
Holly <hharnack@hotmail.com>
2020-06-12 17:47:08 -0600

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,6 +14,7 @@ class Navbar extends React.Component {
                             <a className="nav-item nav-link" href="#about">About Me</a>
                             <a className="nav-item nav-link" href="#proj">Projects</a>
                             <a className="nav-item nav-link" href="#contact">Contact Me</a>
+                            {' '}<div style={{color:"#F1F2EB"}}>|</div> {' '} 
                             <a className="nav-item nav-link" href={pdf}>Resume</a>
                             <a className="nav-item nav-link" href="https://github.com/hharnack/">Github</a>
                             <a className="nav-item nav-link" href="https://www.linkedin.com/in/hollyharnack/">LinkedIn</a>
