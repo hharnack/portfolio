7686a19003d8789184113421f5c06a7dbb6d35e4
Holly <hharnack@hotmail.com>
2020-06-15 13:40:29 -0600

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,14 +1,25 @@
-import React from'react';
+import React from 'react';
+import { Card, Button, Row, Col } from 'react-bootstrap';
 
-class Project extends React.Component{
+class Project extends React.Component {
 
     render() {
 
         return (
             <div className="d-flex justify-content-center">
-            <div className="page">
-               Project 
-            </div>
+                <div className="page">
+                    <div className="title">My Projects</div>
+                    <hr style={{ borderColor: '#556246' }} />
+                    // <Card>
+                    //     <Card.Img top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                    //     <Card.Body>
+                    //         <Card.Title>Card title</Card.Title>
+                    //         <Card.Subtitle>Card subtitle</Card.Subtitle>
+                    //         <Card.Text>Some quick example text to build on the card title and make up the bulk of the cards content.</Card.Text>
+                    //         <Button>Button</Button>
+                    //     </Card.Body>
+                    // </Card>
+                </div>
             </div>
         );
 
