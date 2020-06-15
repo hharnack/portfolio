7585022da03419b216829f2e5fbe9e59c3ccddda
Holly <hharnack@hotmail.com>
2020-06-15 16:22:37 -0600

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ class Contact extends React.Component {
     render() {
 
         return (
-            <div className="d-flex justify-content-center">
+            <div className=" mt-4 d-flex justify-content-center">
                 <div className="page">
                     <div className="title">Contact Me</div>
                     <hr style={{ borderColor: '#556246' }} />
