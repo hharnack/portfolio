a1c581aacaf63b00e0e9a57044548f62e0100f93
Holly <hharnack@hotmail.com>
2020-10-05 21:45:22 -0600

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,6 +4,7 @@ import proj1 from '../images/proj1.JPG';
 import proj2 from '../images/proj2.JPG';
 import proj3 from '../images/proj3.JPG';
 import proj4 from '../images/proj4.JPG';
+import proj5 from '../images/proj5.jpg';
 
 class Project extends React.Component {
 
@@ -48,6 +49,30 @@ class Project extends React.Component {
                                     <Card.Title>Client/Property Management System</Card.Title>
                                     <Card.Text>This is a GUI written in Java that manages clients and properties. It uses awt and connects to a SQL database using JDBC.</Card.Text>
                                     <Card.Link href="https://github.com/hharnack/Management-Graphical-User-Interface">See the Code</Card.Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col md="6" sm="12" xs="12">
+                            <Card className="text-center card-item">
+                                <Card.Img top width="50%" src={proj4} />
+                                <Card.Body>
+                                    <Card.Title>Car Rental</Card.Title>
+                                    <Card.Text>Front-end of a website created in JavaScript, HTML and CSS for a car rental service. A user can search through a list of customers then auto fill fields and order a rental for that customer. A receipt is shown afterwards. 
+                                    Data is stored in a JavaScript file of JSON objects.
+                                    </Card.Text>
+                                    <Card.Link href="https://github.com/hharnack/Car-Rental">See the Code</Card.Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md="6" sm="12" xs="12">
+                            <Card className="text-center card-item">
+                                <Card.Img top width="100%" src={proj5} />
+                                <Card.Body>
+                                    <Card.Title>Currency Converter</Card.Title>
+                                    <Card.Text>This is a currency converter written in JavaScript, HTML and CSS.</Card.Text>
+                                    <Card.Link href="https://github.com/hharnack/CurrencyConverter">See the Code</Card.Link>
                                 </Card.Body>
                             </Card>
                         </Col>
